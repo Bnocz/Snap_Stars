f5b892823496f294124c9fc717fbd322ea4e1a23
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2019-10-10 23:07:53 -0700

    Added signing in with google

--- app/src/main/java/com/example/scrapp/CreateAccActivity.java
+++ app/src/main/java/com/example/scrapp/CreateAccActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+/*Do we need this class? LoginActivity can sign you into google. -Mike*/
 public class CreateAccActivity extends AppCompatActivity {
 
     @Override
